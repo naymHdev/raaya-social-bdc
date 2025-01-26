02defc86f2340c6e546aabdc7f2c62943d2e029f
MacBook Pro <naym100m@gmail.com>
2025-01-26 11:16:26 +0600

    [create responsive] for mobile and pc device

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ document.querySelectorAll(".faq-question").forEach((question) => {
     });
   });
 });
+
+// Business Video Sections carousel
+const carousel = document.querySelector('.carousel');
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+let index = 0;
+
+nextButton.addEventListener('click', () => {
+    if (index < carousel.children.length - 1) {
+        index++;
+    } else {
+        index = 0;
+    }
+    updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+    } else {
+        index = carousel.children.length - 1;
+    }
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const itemWidth = document.querySelector('.carousel-item').clientWidth + 20; // 20px for margin
+    carousel.style.transform = `translateX(-${index * itemWidth}px)`;
+}
